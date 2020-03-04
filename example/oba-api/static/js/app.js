3bb7d9465e95bcb71ee4688652346056c1f1fd06
LarsBreuren <larsbreuren@gmail.com>
2020-03-04 10:26:52 +0100

    adjusted Js

--- example/oba-api/static/js/app.js
+++ example/oba-api/static/js/app.js
@@ -1,43 +1,48 @@
-/*** Fetching data -> refactor into module later ***/
-const section = document.querySelector('section');
-const cors = 'https://cors-anywhere.herokuapp.com/';
-const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
-const query = 'tolkien';
-const key = 'dc0e2f073c03758140452044906bc818';
-const secret = '4289fe6b02878111c06660300798cf4c40a685c4e962a33118340c888699438d';
-const detail = 'Default';
-const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
+var submit = document.getElementById("submit")
+submit.addEventListener("click", function setQuery() {
+  
+  const main = document.querySelector('main');
+  const cors = 'https://cors-anywhere.herokuapp.com/';
+  const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
+  let query = document.getElementById("userInput").value;
+  const key = '1e19898c87464e239192c8bfe422f280';
+  const secret = '4289fec4e962a33118340c888699438d';
+  const detail = 'Default';
+  const pagesize = 5;
+  const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json&pagesize=${pagesize}`;
+  
 
+  const config = {
+    Authorization: `Bearer ${secret}`
+  };
 
-const config = {
-  Authorization: `Bearer ${secret}`
-};
+  fetch(url, config)
+    .then(response => {
+      return response.json();
+    })
 
-fetch(url, config)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    render(data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(data => {
+      render(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
-// render data
-function render(data) {
-  const results = data.results;
-  console.dir(results);
-  results.forEach((item, i) => {
-    const html = `
-            <article>
+  // render data
+  function render(data) {
+    const results = data.results;
+    console.dir(results);
+    results.forEach((item, i) => {
+      const html = `
+            <article class="item">
               <h2>${item.titles[0]}</h2>
+              <p>${item.summaries ? item.summaries[0] : 'Geen samenvatting'}</p>
+              <img src="${
+                item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
+              }">
             </article>
           `;
-    section.insertAdjacentHTML('beforeend', html);
-  });
-}
-     // <p>${item.summaries ? item.summaries[0] : 'Geen samenvatting'}</p>
-    //  <img src="${
-    //   item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
-    // }">
+      main.insertAdjacentHTML('beforeend', html);
+    });
+  }
+})
